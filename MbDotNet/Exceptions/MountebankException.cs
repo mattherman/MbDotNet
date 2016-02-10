77287e33252c6dea3ff5dbc1c4836d3610c4691b
Matt Herman <mdewaddict11@gmail.com>
2016-02-09 21:29:06 -0600

    Added MountebankException

--- MbDotNet/Exceptions/MountebankException.cs
+++ MbDotNet/Exceptions/MountebankException.cs
@@ -6,5 +6,10 @@ namespace MbDotNet.Exceptions
     public class MountebankException : Exception
     {
         public MountebankException(string message) : base(message) { }
+
+        public void Test()
+        {
+            var obj = new { Key = "x", Value = "y" };
+        }
     }
 }
