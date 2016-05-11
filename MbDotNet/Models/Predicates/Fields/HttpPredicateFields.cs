27641f7af18233b6fdc788def1d2ae741f00b1a0
Matt Herman <mdewaddict11@gmail.com>
2016-05-10 23:01:01 -0500

    Added the requestFrom HTTP predicate field

--- MbDotNet/Models/Predicates/Fields/HttpPredicateFields.cs
+++ MbDotNet/Models/Predicates/Fields/HttpPredicateFields.cs
@@ -10,21 +10,42 @@ namespace MbDotNet.Models.Predicates.Fields
 {
     public class HttpPredicateFields : PredicateFields
     {
+        /// <summary>
+        /// The path of the request, without the querystring
+        /// </summary>
         [JsonProperty("path", NullValueHandling = NullValueHandling.Ignore)]
         public string Path { get; set; }
 
+        /// <summary>
+        /// The request body
+        /// </summary>
         [JsonProperty("body", NullValueHandling = NullValueHandling.Ignore)]
         public string RequestBody { get; set; }
 
         [JsonProperty("method", NullValueHandling = NullValueHandling.Ignore)]
         private string RawMethod { get { return Method.HasValue ? Method.Value.ToString().ToUpper() : null; } }
 
+        /// <summary>
+        /// The request method
+        /// </summary>
         public Method? Method { get; set; }
 
+        /// <summary>
+        /// The HTTP headers
+        /// </summary>
         [JsonProperty("headers", NullValueHandling = NullValueHandling.Ignore)]
         public IDictionary<string, string> Headers { get; set; }
 
+        /// <summary>
+        /// The querystring of the request
+        /// </summary>
         [JsonProperty("query", NullValueHandling = NullValueHandling.Ignore)]
         public IDictionary<string, string> QueryParameters { get; set; }
+
+        /// <summary>
+        /// The client socket, primarily used for logging and debugging
+        /// </summary>
+        [JsonProperty("requestFrom", NullValueHandling = NullValueHandling.Ignore)]
+        public string RequestFrom { get; set; }
     }
 }
