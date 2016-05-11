0af0f2f909bf3c09cb5ff29671c0a33a0d4dc7f4
Matt Herman <mdewaddict11@gmail.com>
2016-05-11 11:08:01 -0500

    Added ContainsPredicate and tests

---
+++ MbDotNet.Tests/Models/Predicates/ContainsPredicateTests.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using MbDotNet.Enums;
+using MbDotNet.Models.Predicates;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MbDotNet.Tests.Models.Predicates
+{
+    [TestClass]
+    public class ContainsPredicateTests : PredicateTestBase
+    {
+        [TestMethod]
+        public void Constructor_SetsFieldObject()
+        {
+            var expectedFields = new TestPredicateFields();
+            var predicate = new ContainsPredicate<TestPredicateFields>(expectedFields);
+            Assert.AreSame(expectedFields, predicate.Fields);
+        }
+
+        [TestMethod]
+        public void Constructor_SetsCaseSensitivity()
+        {
+            var fields = new TestPredicateFields();
+            var predicate = new ContainsPredicate<TestPredicateFields>(fields, true, null, null);
+            Assert.IsTrue(predicate.IsCaseSensitive);
+        }
+
+        [TestMethod]
+        public void Constructor_SetsExceptExpression()
+        {
+            const string expectedExceptRegex = "!$";
+
+            var fields = new TestPredicateFields();
+            var predicate = new ContainsPredicate<TestPredicateFields>(fields, false, expectedExceptRegex, null);
+            Assert.AreEqual(expectedExceptRegex, predicate.ExceptExpression);
+        }
+
+        [TestMethod]
+        public void Constructor_SetsXpathSelector()
+        {
+            var expectedXPathSelector = new XPathSelector("!$");
+
+            var fields = new TestPredicateFields();
+            var predicate = new ContainsPredicate<TestPredicateFields>(fields, false, null, expectedXPathSelector);
+            Assert.AreEqual(expectedXPathSelector, predicate.Selector);
+        }
+    }
+}
---
+++ MbDotNet/Models/Predicates/ContainsPredicate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using MbDotNet.Enums;
+using Newtonsoft.Json;
+using MbDotNet.Models.Predicates.Fields;
+
+namespace MbDotNet.Models.Predicates
+{
+    public class ContainsPredicate<T> : PredicateBase where T : PredicateFields, new()
+    {
+        [JsonProperty("equals")]
+        public T Fields { get; private set; }
+
+        public ContainsPredicate(T fields)
+        {
+            Fields = fields;
+        }
+
+        public ContainsPredicate(T fields, bool isCaseSensitive, string exceptExpression, XPathSelector selector)
+            : base(isCaseSensitive, exceptExpression, selector)
+        {
+            Fields = fields;
+        }
+    }
+}
