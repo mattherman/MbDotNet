ebd33457a6c8fd0ad49af09a0ce82ccce22f0661
nrjohnstone <locotewz@gmail.com>
2016-12-08 09:01:58 +0100

    Add acceptance test console application

---
+++ MbDotNet.Acceptance.Tests/AcceptanceTest.cs
@@ -0,0 +1,56 @@
+﻿
+using System;
+using System.Net;
+using FluentAssertions;
+using MbDotNet.Exceptions;
+
+namespace MbDotNet.Acceptance.Tests
+{
+    internal class AcceptanceTest
+    {
+        private readonly MountebankClient _client;
+
+        public AcceptanceTest(MountebankClient client)
+        {
+            _client = client;
+        }
+
+        public void CanCreateImposter()
+        {
+            DeleteAllImposters();
+            CreateImposter();
+            VerifyImposterHasBeenCreated();
+        }
+
+        private void DeleteAllImposters()
+        {
+            _client.DeleteAllImposters();
+        }
+
+        private void VerifyImposterHasBeenCreated()
+        {
+            MountebankException exception = null;
+            try
+            {
+                var imposter = _client.CreateHttpImposter(6000);
+                imposter.AddStub().ReturnsStatus(HttpStatusCode.BadRequest);
+                _client.Submit();
+            }
+            catch (MountebankException e)
+            {
+                exception = e;
+                
+            }
+
+            exception.Should().NotBeNull("Expected imposter to already exist on port");
+            exception.Message.Should().Contain("port is already in use", "Expected imposter to already exist on port");
+        }
+
+        private void CreateImposter()
+        {
+            var imposter = _client.CreateHttpImposter(6000);
+            imposter.AddStub().ReturnsStatus(HttpStatusCode.BadRequest);
+            _client.Submit();
+        }
+    }
+}
---
+++ MbDotNet.Acceptance.Tests/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MbDotNet.Acceptance.Tests
+{
+    class Program
+    {
+        public static MountebankClient Client { get; set; }
+
+        static void Main(string[] args)
+        {
+            SetupTestEnvironment();
+
+            RunAcceptanceTests();
+        }
+
+        private static void RunAcceptanceTests()
+        {
+            new AcceptanceTest(Client).CanCreateImposter();
+        }
+
+        private static void SetupTestEnvironment()
+        {
+            Client = new MountebankClient();
+        }
+    }
+}
