490693c161686018e5de0d19e3d76a71861464a1
nrjohnstone <locotewz@gmail.com>
2016-12-09 10:58:00 +0100

    refactor scope to public for test method

--- MbDotNet.Acceptance.Tests/AcceptanceTest.cs
+++ MbDotNet.Acceptance.Tests/AcceptanceTest.cs
@@ -6,7 +6,7 @@ namespace MbDotNet.Acceptance.Tests
 {
     internal static class AcceptanceTest
     {
-        internal static void CanCreateImposter(MountebankClient client) => new CanCreateImposterTest(client).Run();
+        public static void CanCreateImposter(MountebankClient client) => new CanCreateImposterTest(client).Run();
         public static void CanDeleteImposter(MountebankClient client) => new CanDeleteImposter(client).Run();
     }
 }
