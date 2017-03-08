eb474c5d7062ffa219f9795192ba1b0dcba22a3b
SuperDrew <a.vector@gmail.com>
2017-03-08 21:34:49 +0000

    Add Https Imposter

---
+++ MbDotNet/Models/Imposters/HttpsImposter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using MbDotNet.Enums;
+using MbDotNet.Interfaces;
+using MbDotNet.Models.Stubs;
+using Newtonsoft.Json;
+
+namespace MbDotNet.Models.Imposters
+{
+    public class HttpsImposter : Imposter 
+    {
+        [JsonProperty("stubs")]
+        public ICollection<HttpStub> Stubs { get; private set; }
+
+        public HttpsImposter(int port, string name) : base(port, MbDotNet.Enums.Protocol.Https, name)
+        {
+            Stubs = new List<HttpStub>();
+        }
+
+        public HttpStub AddStub()
+        {
+            var stub = new HttpStub();
+            Stubs.Add(stub);
+            return stub;
+        }
+    }
+}
