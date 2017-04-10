2c56597a305a19dc1fcd60effe537afb7e25b788
Gitter.Jeff <jeff.gitter@we-energies.com>
2017-04-10 10:07:13 -0500

    Added a new response model that contains only the port.  This is used by the request proxy for the purpose of retrieving the port number to set on the imposter object that was submitted.

---
+++ MbDotNet/Models/Responses/CreateImposterResponse.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MbDotNet.Models.Responses
+{
+    internal class CreateImposterResponse
+    {
+        /// <summary>
+        /// The port the imposter is set up to accept requests on.
+        /// </summary>
+        [JsonProperty("port")]
+        public int Port { get; internal set; }
+
+    }
+}
