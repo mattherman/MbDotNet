0a9d8b57de90b1054380a0aa7531a6b1ee0d84e4
Matthew Herman <mattherman11@gmail.com>
2017-10-11 18:57:30 -0500

    Added Or predicate

---
+++ MbDotNet.Tests/Models/Predicates/OrPredicateTests.cs
@@ -0,0 +1,22 @@
+﻿using MbDotNet.Models.Predicates;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace MbDotNet.Tests.Models.Predicates
+{
+    [TestClass]
+    public class OrPredicateTests : PredicateTestBase
+    {
+        [TestMethod]
+        public void OrPredicate_Constructor_SetsPredicateCollection()
+        {
+            var expectedPredicates = new List<PredicateBase>
+            {
+                new EqualsPredicate<TestPredicateFields>(null),
+                new MatchesPredicate<TestPredicateFields>(null),
+            };
+            var predicate = new OrPredicate(expectedPredicates);
+            Assert.AreSame(expectedPredicates, predicate.Predicates);
+        }
+    }
+}
---
+++ MbDotNet/Models/Predicates/OrPredicate.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace MbDotNet.Models.Predicates
+{
+    public class OrPredicate : PredicateBase 
+    {
+        [JsonProperty("or")]
+        public IEnumerable<PredicateBase> Predicates { get; private set; }
+
+        public OrPredicate(IEnumerable<PredicateBase> predicates)
+        {
+            Predicates = predicates;
+        }
+    }
+}
