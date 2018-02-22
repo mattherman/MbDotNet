6760f006a1688246ae7a9f24422de4b63758adbe
Tom.Gould <tom.gould@just-eat.com>
2018-02-22 15:33:17 +0000

    Add ability to create Proxy imposter

---
+++ MbDotNet/Models/Predicates/Fields/PredicateGeneratorFields.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using MbDotNet.Enums;
+using Newtonsoft.Json;
+
+namespace MbDotNet.Models.Predicates.Fields
+{
+    public class PredicateGeneratorFields : PredicateFields
+    {
+        /// <summary>
+        /// The path of the request, without the querystring
+        /// </summary>
+        [JsonProperty("path", NullValueHandling = NullValueHandling.Ignore)]
+        public bool? Path { get; set; }
+
+        /// <summary>
+        /// The request body
+        /// </summary>
+        [JsonProperty("body", NullValueHandling = NullValueHandling.Ignore)]
+        public bool? RequestBody { get; set; }
+
+        [JsonProperty("method", NullValueHandling = NullValueHandling.Ignore)]
+        private bool? RawMethod => Method;
+
+        /// <summary>
+        /// The request method
+        /// </summary>
+        [JsonIgnore]
+        public bool? Method { get; set; }
+
+        /// <summary>
+        /// The HTTP headers
+        /// </summary>
+        [JsonProperty("headers", NullValueHandling = NullValueHandling.Ignore)]
+        public bool? Headers { get; set; }
+
+        /// <summary>
+        /// The querystring of the request
+        /// </summary>
+        [JsonProperty("query", NullValueHandling = NullValueHandling.Ignore)]
+        public bool? QueryParameters { get; set; }
+
+        /// <summary>
+        /// The client socket, primarily used for logging and debugging
+        /// </summary>
+        [JsonProperty("requestFrom", NullValueHandling = NullValueHandling.Ignore)]
+        public bool? RequestFrom { get; set; }
+    }
+}
