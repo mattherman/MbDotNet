9f70778bffdd3afdeefac547f4ca4942d7029c87
Matthew Herman <mattherman11@gmail.com>
2019-11-06 12:24:16 -0600

    Duplicate majority of acceptance tests in regular test project

---
+++ MbDotNet.Tests/Acceptance/AcceptanceTestBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MbDotNet.Tests.Acceptance
+{
+	public class AcceptanceTestBase
+	{
+		protected readonly MountebankClient _client;
+
+		public AcceptanceTestBase()
+		{
+			_client = new MountebankClient();
+		}
+	}
+}
