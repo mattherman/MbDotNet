e3b3bf6f7dd308c62d72ef42ca1f183d290ba1c3
Mahmoud Abduo <mahmoud_abduo@hotmail.com>
2020-02-07 14:45:20 +1100

    Added the "Exists" predicate functionality (#72)

---
+++ MbDotNet/Models/Predicates/ExistsPredicate.cs
@@ -0,0 +1,18 @@
+﻿using MbDotNet.Models.Predicates.Fields;
+using Newtonsoft.Json;
+
+namespace MbDotNet.Models.Predicates
+{
+    public class ExistsPredicate<T> : PredicateBase where T : PredicateFields
+    {
+        [JsonProperty("exists")]
+        public T Fields { get; private set; }
+
+        public ExistsPredicate(T fields, bool isCaseSensitive = false, string exceptExpression = null,
+                                    XPathSelector xpath = null, JsonPathSelector jsonpath = null)
+            : base(isCaseSensitive, exceptExpression, xpath, jsonpath)
+        {
+            Fields = fields;
+        }
+    }
+}
