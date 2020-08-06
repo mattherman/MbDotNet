0c3bd8bc7a8346e134784e36601e795f261e9cde
kichigina.ts <kichigina.ts@skbkontur.ru>
2020-08-06 18:30:04 +0500

    added tests

--- MbDotNet.Tests/Models/Responses/ProxyResponseTests.cs
+++ MbDotNet.Tests/Models/Responses/ProxyResponseTests.cs
@@ -16,5 +16,13 @@ namespace MbDotNet.Tests.Models.Responses
             var response = new ProxyResponse<TestResponseFields>(expectedFields);
             Assert.AreSame(expectedFields, response.Fields);
         }
+
+        [TestMethod]
+        public void TestResponse_Constructor_SetsBehavior()
+        {
+            var behavior = new Behavior();
+            var response = new ProxyResponse<TestResponseFields>(new TestResponseFields(), behavior);
+            Assert.AreSame(behavior, response.Behavior);
+        }
     }
 }
