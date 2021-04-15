1c3fedad2f2d4995bc81d0f605838bff51fbd35a
Adrian Bontea <contact@adrianbontea.com>
2021-04-15 16:37:01 +0300

    Add SmtpResponseFields

---
+++ MbDotNet/Models/Responses/Fields/SmtpResponseFields.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MbDotNet.Models.Responses.Fields
+{
+    public class SmtpResponseFields: ResponseFields
+    {
+    }
+}
