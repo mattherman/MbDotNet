d75fe24abdd30e2e861d74b5a7322688821d6d65
Michael Velasquez <tgmike77@gmail.com>
2022-06-15 13:28:50 -0700

    Added AllowCORS parameter...
    Added AllowCORS parameter to HttpImposter and HttpsImposters. Additionally added tests for the parameter in respective test files.

--- MbDotNet.Tests/Models/Imposters/HttpImposterTests.cs
+++ MbDotNet.Tests/Models/Imposters/HttpImposterTests.cs
@@ -56,6 +56,30 @@ namespace MbDotNet.Tests.Models.Imposters
             Assert.IsNotNull(imposter.DefaultResponse);
         }
 
+        [TestMethod]
+        public void Constructor_InitialAllowCORS()
+        {
+            const bool allowCORS = false;
+            var imposter = new HttpImposter(null, null);
+            Assert.AreEqual(allowCORS, imposter.AllowCORS);
+        }
+
+        [TestMethod]
+        public void Constructor_AllowCORSTrue()
+        {
+            const bool allowCORS = true;
+            var imposter = new HttpImposter(null, null, false, null, true);
+            Assert.AreEqual(allowCORS, imposter.AllowCORS);
+        }
+
+        [TestMethod]
+        public void Constructor_AllowCORSFalse()
+        {
+            const bool allowCORS = false;
+            var imposter = new HttpImposter(null, null, false, null, false);
+            Assert.AreEqual(allowCORS, imposter.AllowCORS);
+        }
+
         #endregion
 
         #region Stub Tests
